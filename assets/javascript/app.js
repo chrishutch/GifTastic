408b25b208a594c2c719b6edce14afa48a136498
Chris Hutchinson <chrisbond19@me.com>
2018-01-18 23:19:06 -0600

    Add Bootstrap styling and GIFs can now be clicked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,48 +1,50 @@
 $(document).ready(function() {
-	var universe = ["Earth", "The Sun", "Pluto", "Comet"];
+	var universe = ["Earth", "Sun", "Pluto", "Comet"];
 
      function renderButtons() {
 
-        // Deleting the movie buttons prior to adding new movie buttons
-        // (this is necessary otherwise we will have repeat buttons)
+        // Deleting the universe buttons prior to adding new universe buttons
         $("#buttons-view").empty();
 
-        // Looping through the array of movies
+        // Looping through the array of universe
         for (var i = 0; i < universe.length; i++) {
 
-          // Then dynamicaly generating buttons for each movie in the array.
-          // This code $("<button>") is all jQuery needs to create the start and end tag. (<button></button>)
+          // Then dynamicaly generating buttons for each universe in the array.
           var a = $("<button>");
           // Adding a class
           a.addClass("universe");
-          // Adding a data-attribute with a value of the movie at index i
+          // Adding a data-attribute with a value of the universe at index i
           a.attr("data-name", universe[i]);
-          // Providing the button's text with a value of the movie at index i
+          a.addClass('btn btn-dark');
+          // Providing the button's text with a value of the universe at index i
           a.text(universe[i]);
-          // Adding the button to the HTML
+          // Adds the button to the HTML
           $("#buttons-view").append(a);
         }
       }
 	renderButtons();
+
       // This function handles events where one button is clicked
       $("#find-space").on("click", function(event) {
-        // event.preventDefault() prevents the form from trying to submit itself.
-        // We're using a form so that the user can hit enter instead of clicking the button if they want
         event.preventDefault();
 
         // This line will grab the text from the input box
         var space = $("#space-input").val().trim();
-        // The space from the textbox is then added to our array
+
+        // The spaceItem from the textbox is then added to our array
         universe.push(space);
 
-        // calling renderButtons which handles the processing of our movie array
+        // Add new button to array
         renderButtons();
       });
 
       // Event listener for all button elements
-    $("button").on("click", function() {
+    $("#buttons-view").on("click", "button", function() {
+    	event.preventDefault();
+
+      //Empties the images before displaying new GIFs
     	$("#images").empty();
-    	console.log("clicked");
+
       // In this case, the "this" keyword refers to the button that was clicked
       var spaceItem = $(this).attr("data-name");
 
@@ -77,9 +79,11 @@ $(document).ready(function() {
               // Creating an image tag
               var universeImage = $("<img>");
 
-              // Giving the image tag an src attribute of a proprty pulled off the
-              // result item
-              universeImage.attr("src", results[i].images.fixed_height.url);
+              // Gives the image tag an src attribute of a proprty pulled off the
+              // Still Image
+              universeImage.attr("src", results[i].images.fixed_height_still.url);
+              // Animated Image
+              universeImage.attr('animated', results[i].images.fixed_height.url);
 
               // Appending the paragraph and universeImage we created to the "gifDiv" div we created
               gifDiv.append(p);
@@ -90,5 +94,12 @@ $(document).ready(function() {
             }
           };
       });
-  });    
+  });  
+    //Click on GIFs to change from still to animated
+      $("#images").on("click", "img", function() {
+        var m = $(this).attr('src');
+        $(this).attr('src', $(this).attr('animated'));
+        $(this).attr('animated', m);
+
+      });  
 });
